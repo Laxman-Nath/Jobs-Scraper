607917f9bdb3168212d8f7ca2cd1df747fcd222c
Laxman-Nath <nathlaxman242@gmail.com>
2026-07-23 14:45:22 +0545

    fix: add frontend url to solve cors issue

--- job-scraper-backend/src/main/java/com/laxmannath/job_scraper_backend/configs/CorsConfig.java
+++ job-scraper-backend/src/main/java/com/laxmannath/job_scraper_backend/configs/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000","https://jobs-scraper-nu.vercel.app")
                         .allowedMethods("GET", "POST", "PATCH", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
